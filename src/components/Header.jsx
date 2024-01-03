a8e1473583d7a00616f9a7fc469f0798b21fcb18
azizx018 <chiara.aziz@gmail.com>
2024-01-03 17:39:53 -0600

    added Header with font and icon

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1> 
+                Books
+                <AutoStoriesIcon/>
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App';
 
-ReactDOM.render(
-  <h1>Hello world</h1>,
-  document.getElementById("root")
-);
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById("root")
 // );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
